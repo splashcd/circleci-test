01f7996037c2c727f699a8dd9730917251e5b768
Brett Berry <brett.berry@getordermark.com>
2022-03-04 15:17:37 -0700

    Adding files

---
+++ config/settings.py
@@ -0,0 +1,27 @@
+class AppConfig:
+    DB: str
+    logging = "Test"
+
+    def __init__(self):
+        print("In AppConfig")
+
+
+class DevConfig(AppConfig):
+    DB = "dev"
+
+    def __init__(self):
+        print(" InDevConfig")
+
+
+class StagingConfig(AppConfig):
+    DB = "staging"
+
+    def __init__(self):
+        print("In StagingConfig")
+
+
+class ProdConfig(AppConfig):
+    DB = "Prod"
+
+    def __init__(self):
+        print(f"In ProdConfig")
