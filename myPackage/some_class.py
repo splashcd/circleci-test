e72c9ded44c970edc27972a957fbca5746c58682
Brett Berry <brett.berry@getordermark.com>
2022-03-08 16:05:06 -0700

    Fixed pre-commit errors

--- myPackage/some_class.py
+++ myPackage/some_class.py
@@ -1,21 +1,20 @@
-import logging
 import json
+import logging
 from typing import Optional, Union
 
 
 class SomeClass:
     def __init__(self):
-        self._logger =    logging.getLogger()
+        self._logger = logging.getLogger()
 
     def _print_json(self, arg1: dict) -> None:
-        self._logger.info( f"{json.dumps(arg1)}" )
+        self._logger.info(f"{json.dumps(arg1)}")
         return None
 
     def eval_arg(self, arg1: Optional[Union[dict, str]] = None) -> None:
         if isinstance(arg1, str):
             payload = {"key": arg1}
-        elif isinstance(arg1, dict) and \
-          arg1["key"]:
+        elif isinstance(arg1, dict) and arg1["key"]:
             payload = arg1
         else:
             payload = {"key": "default"}
